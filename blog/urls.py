09f8056bb542fafc1d0b42320838e8bb69bd044a
Eva Scholten <eva.anna.scholten@gmail.com>
2018-11-01 12:21:37 +0100

    Added views to create/edit blogpost inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     # django will transfer the integer to a view as a variable called pk
     # at http://127.0.0.1:8000/post/5/ django knows you look for a view called post_detail and transfers the information that pk=5 to that view
     # next: add the view in views.py
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 # The views file connects models to templates
 
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
 #Import the model of the posts, so the parts it consists of (name, author, etc)
 from .models import Post
 # when you ander a pk value that does not exist you get a nice error message 
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 
 # views are below
@@ -24,4 +26,32 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
     # Refers to the post_detail.html template
+
+#View for new blog entries created inn the custom form
+def post_new(request):
+    if request.method == "POST": #If the method is POST(you want to post something),then construct the form
+        form = PostForm(request.POST) #POST because you're posting data
+        if form.is_valid(): #Check if all required fields in the form are set
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save() #if it's allvalid it can be saved
+            return redirect('post_detail', pk=post.pk) #Go to post detail page of newlycreated page
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
     
